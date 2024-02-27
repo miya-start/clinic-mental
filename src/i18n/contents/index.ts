4ed5b26e0cd5c7595e68570e0c06210e24c7cf56
miya-start <0000000to1@gmail.com>
2024-02-28 02:50:35 +0900

    feat: https://i.gyazo.com/c9cd69f0926fb3b4343b6bcd2084f483.png

--- src/i18n/contents/index.ts
+++ src/i18n/contents/index.ts
@@ -1,11 +1,11 @@
 export const contents = {
   title: {
-    ja: `無料のテンプレート <span class="hidden xl:inline">ウェブサイト作成用</span><span class="text-accent dark:text-white highlight"> Astro 4.0</span> + Tailwind CSS`,
+    ja: '今日より、ずっと心地よい 明日へ',
     en: `Free template for <span class="hidden xl:inline">creating websites with</span>
     <span class="text-accent dark:text-white highlight"> Astro 4.0</span> + Tailwind CSS`,
   },
   subtitle: {
-    ja: '<span class="font-semibold">AstroWind</span> はフリーで、カスタマイズ可能なテンプレート for Astro 4.0 + Tailwind CSS.</span>',
+    ja: '私たちはまず、じっくりとお話を聞くことから始めます。<br />人と暮らしがつながるお手伝いをし<br />最新の医療技術をもとに、「治る」提案をします。',
     en: '<span class="font-semibold">AstroWind</span> is a free, customizable and production-ready template for Astro 4.0 + Tailwind CSS.</span>',
   },
 };
